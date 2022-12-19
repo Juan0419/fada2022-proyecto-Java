5951be8cf86d1d7770311319d9b1e3d323146563
Juan Veloza <juan.veloza@correounivalle.edu.co>
2022-12-19 17:03:26 -0500

    Algo del CSR, falta aun setval y la matriz traspuesta de coordinate

--- app/src/main/java/proyecto/SparseMatrixCSR.java
+++ app/src/main/java/proyecto/SparseMatrixCSR.java
@@ -1,9 +1,11 @@
 package proyecto;
 
 import javax.naming.OperationNotSupportedException;
+
 import lombok.Getter;
 
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 
 public class SparseMatrixCSR {
     private LoadFile loader = LoadFile.getInstance();
@@ -19,26 +21,75 @@ public class SparseMatrixCSR {
         //Load data
         loader.loadFile(inputFile);
         matrix = loader.getMatrix();
+        // Calcular el tamaño de la matriz de columnas
+        int size = 0;
+        for (int[] row : matrix) {
+            for (int value : row) {
+                if (value != 0) {
+                    size++;
+                }
+            }
+        }
+
+        // Se inicializan las matrices de columnas, valores y filas
+        columns = new int[size];
+        values = new int[size];
+        rows = new int[matrix.length + 1];
+
+        // Llena las matrices de columnas, valores y filas
+        size = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            rows[i] = size;
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] != 0) {
+                    values[size] = matrix[i][j];
+                    columns[size] = j;
+                    size++;
+                }
+            }
+        }
+        rows[matrix.length] = size;
+    }
+
+    public int getElement(int i, int j) throws OperationNotSupportedException {
+        /*int jb = rows[i];
+        int pos = columns[jb];
+
+        while (pos != j) {
+
+            jb++;
+            if (jb > columns.length) {
+                pos = 0;
+                break;
+            }
+        }
+        return values[pos];*/
         throw new OperationNotSupportedException();
     }
 
-    public int getElement(int i, int j) throws OperationNotSupportedException
-    {
+    public int[] getRow(int i) throws OperationNotSupportedException {
+        int maxCol = columns[0];
+
+        for (int j : columns) {
+            if (j > maxCol)
+                maxCol = j;
+        }
+        int col_ini = rows[i];
+        int[] filaAns = new int[maxCol + 1];
+
+
+        for (int colAux = col_ini; colAux < rows[i + 1]; colAux++) {
+            int var = columns[colAux];
+            filaAns[var] = values[colAux];
+        }
+        return filaAns;
+    }
+
+    public int[] getColumn(int j) throws OperationNotSupportedException {
         throw new OperationNotSupportedException();
     }
 
-    public int[] getRow(int i) throws OperationNotSupportedException
-    {
-        throw new OperationNotSupportedException();
-    }
-
-    public int[] getColumn(int j) throws OperationNotSupportedException
-    {
-        throw new OperationNotSupportedException();
-    }
-
-    public void setValue(int i, int j, int value) throws OperationNotSupportedException
-    {
+    public void setValue(int i, int j, int value) throws OperationNotSupportedException {
         throw new OperationNotSupportedException();
     }
 
@@ -46,8 +97,7 @@ public class SparseMatrixCSR {
      * This method returns a representation of the Squared matrix
      * @return object that contests the squared matrix;
      */
-    public SparseMatrixCSR getSquareMatrix() throws OperationNotSupportedException
-    {
+    public SparseMatrixCSR getSquareMatrix() throws OperationNotSupportedException {
         SparseMatrixCSR squaredMatrix = new SparseMatrixCSR();
         throw new OperationNotSupportedException();
     }
@@ -56,8 +106,7 @@ public class SparseMatrixCSR {
      * This method returns a representation of the transposed matrix
      * @return object that contests the transposed matrix;
      */
-    public SparseMatrixCSR getTransposedMatrix() throws OperationNotSupportedException
-    {
+    public SparseMatrixCSR getTransposedMatrix() throws OperationNotSupportedException {
         SparseMatrixCSR squaredMatrix = new SparseMatrixCSR();
         throw new OperationNotSupportedException();
     }
